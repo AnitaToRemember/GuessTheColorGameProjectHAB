921f7371670b52011012922f695cc64c794e3fec
Eve <eve@Eves-MacBook-Pro.local>
2023-06-20 22:39:08 +0200

    set backgroundColor on box

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,13 @@
 'use strict'
 
-const colorRandom = () => {
-    const randomNumber = Math.floor(Math.random() * 255) + 1;
+const randomNumber = (maxRange) => {
+    const randomNumber = Math.floor(Math.random() * maxRange) + 1;
     return randomNumber;
 }
 
-
-let redRandom = colorRandom();
-let greenRandom = colorRandom();
-let blueRandom = colorRandom();
+let redRandom = randomNumber(255);
+let greenRandom = randomNumber(255);
+let blueRandom = randomNumber(255);
 let colorRGB = 'rgb(' + redRandom + ', ' + greenRandom + ', ' + blueRandom + ')';
 let hits = 0;
 let failures = 0;
@@ -20,25 +19,44 @@ const blueColorSpan = document.getElementById("random_blue");
 const hitsSpan = document.getElementById("hits");
 const failuresSpan = document.getElementById("failures");
 
+const randomFakeColour = () => {
+    const red = randomNumber(255);
+    const green = randomNumber(255);
+    const blue = randomNumber(255);
+    const newColor = 'rgb(' + red + ', ' + green + ', ' + blue + ')';
+    return newColor;
+}
+
+const createBlockColor = () => {
+    const trueBoxNumber = randomNumber(6);
+    for (let i = 1; i <= 6; i++) {
+        const box = document.getElementById(`colour${i}`);
+        if (trueBoxNumber === i) {
+            box.style.backgroundColor = colorRGB;
+        }else{
+            box.style.backgroundColor = randomFakeColour();
+        }
+        
+    }
+}
+
 const playGame = () => {
-    redRandom = colorRandom();
+    redRandom = randomNumber(255);
     redColorSpan.innerText = redRandom;
-    greenRandom = colorRandom();
+    greenRandom = randomNumber(255);
     greenColorSpan.innerText = greenRandom;
-    blueRandom = colorRandom();
+    blueRandom = randomNumber(255);
     blueColorSpan.innerText = blueRandom;
     colorRGB = 'rgb(' + redRandom + ', ' + greenRandom + ', ' + blueRandom + ')';
-    console.log(colorRGB);
     hits = 0;
     hitsSpan.innerText = hits;
     failures = 0;
     failuresSpan.innerText = failures;
+    createBlockColor();
 }
 
 playButton.addEventListener("click", playGame);
 playGame();
 
-const createBlockColor = () => {
-    //añadir el color de fondo verdadero a una de las cajas de forma aleatoria y a las otras asignarles otros randoms
-}
+
 
